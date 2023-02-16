3c7c939a00ff20c7783f1d96160c1f54fe5dc35c
Razumova Tanya <razumova08@gmail.com>
2023-02-16 23:21:02 +0300

    whole page layout

--- sliders-js.js
+++ sliders-js.js
@@ -1,28 +1,41 @@
-var swiper = new Swiper(".swiper", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
 
-var buttonMore = document.querySelector('.main__showmoreButton');
-var buttonLess = document.querySelector('.main__showlessButton');
+
+var buttonMore = document.querySelector('.showmoreButton');
+var buttonLess = document.querySelector('.showlessButton');
 var icon = document.querySelector('.main__button');
+
 var sony = document.querySelector('.swiper__slide--sony');
 var viewsonic = document.querySelector('.swiper__slide--viewsonic');
 
-
+var buttonMore2 = document.querySelector('.showmore--fixing');
+var buttonLess2 = document.querySelector('.showless--fixing');
+var icon2 = document.querySelector('.icon--fixing');
+var display = document.querySelector('.fixing__button:nth-child(4)');
 var wrapper = document.querySelector('.swiper__wrapper');
 var template = document.querySelector('#element__template').content;
 var element = template.querySelector('li');
 var clonedElement1 = element.cloneNode(true);
 var clonedElement2 = element.cloneNode(true);
+
+
 element.classList.add('swiper__slide--lenovo');
 clonedElement1.classList.add('swiper__slide--samsung');
 clonedElement2.classList.add('swiper__slide--apple');
 
+var on = function () {
+  buttonLess.classList.remove('button--hidden'); 
+  icon.classList.toggle('icon__rotate');   
+  buttonMore.classList.add('button--hidden');
+};
+
+var off = function () {
+  buttonMore.classList.remove('button--hidden'); 
+  icon.classList.toggle('icon__rotate');  
+  buttonLess.classList.add('button--hidden');
+}
+
 function click() {
 buttonMore.addEventListener('click',  () => {
-   
    
     wrapper.appendChild(element);
     wrapper.appendChild(clonedElement1);
@@ -30,25 +43,36 @@ buttonMore.addEventListener('click',  () => {
     sony.classList.remove('swiper--hidden');
     viewsonic.classList.remove('swiper--hidden');
     buttonLess.classList.remove('button--hidden'); 
-    icon.classList.toggle('icon__rotate');   
-    buttonMore.classList.add('button--hidden');
-   
-
+  icon.classList.toggle('icon__rotate');   
+  buttonMore.classList.add('button--hidden');
 });
     buttonLess.addEventListener('click', function () {
 
-        sony.classList.add('swiper--hidden');
-        sony.classList.remove('swiper--visible');
-        buttonMore.classList.remove('button--hidden'); 
-        icon.classList.toggle('icon__rotate');  
-        buttonLess.classList.add('button--hidden');
         sony.classList.add('swiper--hidden');
         viewsonic.classList.add('swiper--hidden');
         element.remove();
         clonedElement1.remove();
         clonedElement2.remove();
+        buttonMore.classList.remove('button--hidden'); 
+        icon.classList.toggle('icon__rotate');  
+        buttonLess.classList.add('button--hidden');
     });
   
 }
 click();
    
+
+buttonMore2.addEventListener('click',  () => {
+  buttonLess2.classList.remove('button--hidden'); 
+  icon2.classList.toggle('icon__rotate');   
+  buttonMore2.classList.add('button--hidden');
+  display.classList.remove('button--hidden');
+  });
+
+  buttonLess2.addEventListener('click',  () => {
+    buttonMore2.classList.remove('button--hidden'); 
+  icon2.classList.toggle('icon__rotate');  
+  buttonLess2.classList.add('button--hidden');
+  display.classList.add('button--hidden');
+      });
+    
